49d501007acd4ea4db7296c3fa4f0816818939fd
dong young <dong5854@gmail.com>
2023-01-10 00:39:55 +0900

    feat: myTheme 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes
+} from "react-router-dom"
+import Home from "./routes/Home"
+import { ThemeProvider } from "@mui/material";
+import myTheme from "themes/myTheme";
+
 function App() {
-	return <div>Hello World!</div>;
+	return (
+		<ThemeProvider theme={myTheme}>
+			<Router>
+				<Routes>
+					<Route path="/" element={<Home/>}/>
+				</Routes>
+			</Router>
+		</ThemeProvider>
+	);
 }
 
 export default App;
---
+++ src/themes/myTheme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const myTheme = createTheme({
+    palette: {
+        primary: {
+            light: '#62BD69',
+            main: '#30694B',
+            dark: '#0C3823',
+            contrastText: '#fff',
+          },
+    }
+});
+
+export default myTheme;
