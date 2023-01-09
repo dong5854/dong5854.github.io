e3eddb6fce9dfd1a82fbecdea76f6835d4edc66b
dong young <dong5854@gmail.com>
2023-01-10 00:38:11 +0900

    feat: Home 페이지 추가

---
+++ src/routes/Home.js
@@ -0,0 +1,10 @@
+import NavigationTabs from "../components/NavigationTabs";
+
+function Home() {
+    return (
+        <>
+            <NavigationTabs></NavigationTabs>
+        </>
+    )
+}
+export default Home;
