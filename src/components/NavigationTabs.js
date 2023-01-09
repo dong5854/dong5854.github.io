b4b0d2aa6bc47e8c260128a555278282087b608e
dong young <dong5854@gmail.com>
2023-01-10 00:37:26 +0900

    feat: navigation tabs 추가

---
+++ src/components/NavigationTabs.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+
+function NavigationTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+      <Tabs value={value} onChange={handleChange} centered>
+        <Tab label="글" />
+        <Tab label="소개" />
+      </Tabs>
+    </Box>
+  );
+}
+export default NavigationTabs;
